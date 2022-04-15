00f064a5b1bcfbdb1fed72c255a1351e57f81da3
Dan <danpetersen@hotmail.co.uk>
2022-04-15 14:46:13 +0100

    test connnection change

--- database/connection.js
+++ database/connection.js
@@ -1,10 +1,17 @@
 const mysql = require("mysql");
 
+// const DATABASE = mysql.createConnection({
+//   host: process.env.DB_HOST || CLEARDB_DB_HOST,
+//   user: process.env.DB_USER || CLEARDB_DB_USER,
+//   password: process.env.DB_PASS || CLEARDB_DB_PASS,
+//   database: "NodeJS_Starter" || CLEARDB_DB_NAME,
+// });
+
 const DATABASE = mysql.createConnection({
-  host: process.env.DB_HOST || CLEARDB_DB_HOST,
-  user: process.env.DB_USER || CLEARDB_DB_USER,
-  password: process.env.DB_PASS || CLEARDB_DB_PASS,
-  database: "NodeJS_Starter" || CLEARDB_DB_NAME,
+  host: CLEARDB_DB_HOST,
+  user: CLEARDB_DB_USER,
+  password: CLEARDB_DB_PASS,
+  database: CLEARDB_DB_NAME,
 });
 
 module.exports = DATABASE;
