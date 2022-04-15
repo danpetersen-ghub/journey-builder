225e88412d9f6337822cecc25bcd44e07dc5f104
Dan <danpetersen@hotmail.co.uk>
2022-04-15 10:24:42 +0100

    cleardb var

--- database/connection.js
+++ database/connection.js
@@ -1,7 +1,7 @@
 const mysql = require("mysql");
 
 const DATABASE = mysql.createConnection({
-  host: process.env.DB_HOST,
+  host: process.env.DB_HOST || CLEARDB_DATABASE_URL,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: "NodeJS_Starter",
