e4b9e7dacfc2a62f8d1c780920b17d96a0d74627
Dan <danpetersen@hotmail.co.uk>
2022-04-15 10:31:46 +0100

    prod db vars added

--- database/connection.js
+++ database/connection.js
@@ -2,8 +2,8 @@ const mysql = require("mysql");
 
 const DATABASE = mysql.createConnection({
   host: process.env.DB_HOST || CLEARDB_DATABASE_URL,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
+  user: process.env.DB_USER || CLEARDB_DB_USER,
+  password: process.env.DB_PASS || CLEARDB_DB_PASS,
   database: "NodeJS_Starter",
 });
 
