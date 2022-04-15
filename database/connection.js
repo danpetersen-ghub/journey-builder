94618601a956735453b51843cee41e1dc0ae040b
Dan <danpetersen@hotmail.co.uk>
2022-04-15 14:18:28 +0100

    host update

--- database/connection.js
+++ database/connection.js
@@ -1,7 +1,7 @@
 const mysql = require("mysql");
 
 const DATABASE = mysql.createConnection({
-  host: process.env.DB_HOST || CLEARDB_DATABASE_URL,
+  host: process.env.DB_HOST || CLEARDB_DB_HOST,
   user: process.env.DB_USER || CLEARDB_DB_USER,
   password: process.env.DB_PASS || CLEARDB_DB_PASS,
   database: "NodeJS_Starter" || CLEARDB_DB_NAME,
