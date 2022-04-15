a80576316617cad534b660d2e05480fa818a78d4
Dan <danpetersen@hotmail.co.uk>
2022-04-15 14:02:24 +0100

    db config vars update

--- database/connection.js
+++ database/connection.js
@@ -4,7 +4,7 @@ const DATABASE = mysql.createConnection({
   host: process.env.DB_HOST || CLEARDB_DATABASE_URL,
   user: process.env.DB_USER || CLEARDB_DB_USER,
   password: process.env.DB_PASS || CLEARDB_DB_PASS,
-  database: "NodeJS_Starter",
+  database: "NodeJS_Starter" || CLEARDB_DB_NAME,
 });
 
 module.exports = DATABASE;
