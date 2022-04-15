eca31b806461f6b46cd830e46ea01498dc9eece2
Dan <danpetersen@hotmail.co.uk>
2022-04-15 15:58:53 +0100

    234

--- database/connection.js
+++ database/connection.js
@@ -1,17 +1,17 @@
 const mysql = require("mysql");
 
-const DATABASE = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: "NodeJS_Starter",
-});
-
 // const DATABASE = mysql.createConnection({
-//   host: CLEARDB_DB_HOST,
-//   user: CLEARDB_DB_USER,
-//   password: CLEARDB_DB_PASS,
-//   database: CLEARDB_DB_NAME,
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASS,
+//   database: "NodeJS_Starter",
 // });
+
+const DATABASE = mysql.createConnection({
+  host: CLEARDB_DB_HOST,
+  user: CLEARDB_DB_USER,
+  password: CLEARDB_DB_PASS,
+  database: CLEARDB_DB_NAME,
+});
 
 module.exports = DATABASE;
