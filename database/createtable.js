9bd580fc60c7865bf39aa29193e236d4ed40fdd0
Dan <danpetersen@hotmail.co.uk>
2023-07-21 10:09:46 +0100

    Additonal Fields in DB

--- database/createtable.js
+++ database/createtable.js
@@ -12,9 +12,20 @@ connection.connect(function (err) {
   if (err) throw err;
   console.log("Connected to MySQL DB");
 
-  let sqlTable = `CREATE TABLE items (id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, column1 VARCHAR(255), column2 VARCHAR(255)  )`;
+  let SQLTable = `
+  CREATE TABLE items (
+    id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    column1 VARCHAR(255),
+    column2 VARCHAR(255),
+    requestor_name VARCHAR(255) NULL,
+    requestor_email VARCHAR(255) NULL,
+    email_name VARCHAR(255) NULL,
+    program_name VARCHAR(255) NULL,
+    createdDate TIMESTAMP NULL DEFAULT NOW()
+);
+  `;
 
-  connection.query(sqlTable, function (err, result) {
+  connection.query(SQLTable, function (err, result) {
     if (err) {
       console.log("Table might already exist");
       console.log(err);
