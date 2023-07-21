2ed3508f49b1c047866197a92788e7154aff084d
Dan <danpetersen@hotmail.co.uk>
2023-07-21 19:19:54 +0100

    fix

--- public/script.js
+++ public/script.js
@@ -1,5 +1,5 @@
-import { sendLoginCreds, getAllRecords, createRecord, deleteRecord, updateRecord } from './modules/api.mjs';
-import { displayDataTable, contentArea, addToNavUI, showRequestForm, showEmailBriefForm, displayRecord } from './modules/ui.mjs';
+import { sendLoginCreds, getAllRecords } from './modules/api.mjs';
+import { contentArea, addToNavUI, showRequestForm } from './modules/ui.mjs';
 import { initializeQuillEditor } from './modules/quill.mjs';
 import { showModules } from './modules/emailTemplates.mjs';
 import { formNavItem, dashboardNavItem, showModulesNavItem } from './modules/nav.mjs'
@@ -29,49 +29,3 @@ loginButton.addEventListener("click", function () {
   console.log("submit hit");
   sendLoginCreds();
 });
-
-//keeping track of sorting order
-let sortOrders = {
-  id: 'desc',
-  column1: 'asc',
-  column2: 'asc',
-};
-
-//This function will sort the rows in the table based on the clicked column
-function sortTable(sortKey, sortOrder) {
-  // Toggle sort order
-  sortOrders[sortKey] = sortOrders[sortKey] === 'desc' ? 'asc' : 'desc';
-  sortOrder = sortOrders[sortKey];
-
-  //send data to api endpoint
-  const headers = {
-    method: "GET",
-    headers: {
-      Accept: "application/json",
-      "Content-Type": "application/json",
-    },
-  };
-
-  //send data to api endpoint to get data
-  const response = fetch("/api/all/data", headers);
-  console.log("Sent Req to: /api/all/data");
-
-  //log out the response, then log response
-  response
-    .then((response) => response.json())
-    .then((responseJSON) => {
-      let records = responseJSON;
-      records.sort((a, b) => {
-        if (sortOrder === 'desc') {
-          if (a[sortKey] < b[sortKey]) return -1;
-          if (a[sortKey] > b[sortKey]) return 1;
-          return 0;
-        } else {
-          if (a[sortKey] < b[sortKey]) return 1;
-          if (a[sortKey] > b[sortKey]) return -1;
-          return 0;
-        }
-      });
-      displayDataTable(records);
-    });
-}
